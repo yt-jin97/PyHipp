859d694180433e4f3d0fcc5c36896bc8040559bf
shihcheng <shihcheng@me.com>
2020-09-17 16:57:36 +0800

    Fixed typos.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-	name="PyHippocampus",
+	name="PyHipp",
 	version="0.1",
 	description="""Tools for processing NHP hippocampus data""",
-	url="https://github.com/shihchengyen/PyHippocampus.git",
+	url="https://github.com/shihchengyen/PyHipp.git",
 	author="Shih-Cheng Yen & friends",
 	author_email="shihcheng@nus.edu.sg",
-	packages=["PyHippocampus"],
+	packages=["PyHipp"],
 )
