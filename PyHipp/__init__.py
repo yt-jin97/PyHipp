82aa7b27c6ae8ca07179b7c857f18706e980c139
shihcheng <shihcheng@me.com>
2020-09-17 17:04:15 +0800

    Removed unnecessary modules.

--- PyHipp/__init__.py
+++ PyHipp/__init__.py
@@ -10,14 +10,10 @@ from .helperfunctions import computeFFT, removeLineNoise
 from .aligning_objects import aligning_objects
 from .umaze import Umaze
 from .spiketrain import Spiketrain
-# from .mountain_batch import mountain_batch
-# from .export_mountain_cells import export_mountain_cells
 from .eyelink import EDFSplit 
 from .eyelink import Eyelink
 from .vmlfp import VMLFP
 from .vmhighpass import VMHighPass
 from .vmraw import VMRaw
 from .raycast import raycast
-from .waveform import Waveform
 from .freqspectrum import FreqSpectrum
-from .vmpc import VMPlaceCell
